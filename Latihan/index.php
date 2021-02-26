3e6071ad6bb1a1cd41127d3fcb169da41a8d8072
putuyuliawati10 <79696038+putuyuliawati10@users.noreply.github.com>
2021-02-26 18:28:43 +0800

    Add files via upload

---
+++ Latihan/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Selamat Datang di Website Iluh</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <link rel="canonical" href="http://informatika.undiksha.ac.id/">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body>
+    <div class="jumbotron text-center">
+        <h1>Manfaat Salak Bagi Kesehatan Tubuh</h1>
+        <img src="media\salak1.jpg">
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Meningkatkan Daya Tahan Tubuh</h3>
+                <p>Salah satu manfaat salak adalah meningkatkan imunitas tubuh. Manfaat ini dapat diperoleh karena kandungan vitamin C pada salak mampu membantu memperkuat sistem kekebalan tubuh dan melindungi tubuh dari berbagai macam infeksi.</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Mencegah Sembelit</h3>
+                <p>Masih banyak masyakarat yang percaya bahwa mengonsumsi salak bisa menyebabkan sembelit atau susah buang air besar (BAB). Faktanya, ini hanyalah mitos belaka karena konsumsi salak secukupnya justru dapat membantu mencegah terjadinya sembelit.</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Mencerahkan Kulit</h3>
+                <p>Penelitian menunjukkan bahwa ekstrak salak dapat dimanfaatkan sebagai bahan alami untuk mencerahkan kulit berkat kandungan vitamin C di dalamnya.Senyawa flavonoid yang didapatkan dari ekstrak buah salak juga terbukti efektif untuk menurunkan kadar senyawa pigmen yang memberikan warna pada kulit sehingga dapat dimanfaatkan sebagai bahan krim pemutih kulit.</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Meningkatkan Kesehatan Jantung</h3>
+                <p>Untuk membantu meningkatkan kesehatan jantung, Anda perlu mengonsumsi makanan yang mengandung kalium, seperti salak. Kalium berperan penting dalam menjaga irama detak jantung dan menurunkan tekanan darah, sehingga dapat menjaga kerja jantung tetap optimal.</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Mengontrol Diabetes</h3>
+                <p>Salak mengandung nutrisi yang dibutuhkan penderita diabetes. Penelitian menunjukkan bahwa salak dapat menurunkan kadar kolesterol darah tikus yang diberi makan makanan tinggi lemak. Hal ini sangat baik untuk penderita diabetes yang sering kali mengalami masalah kolesterol.</p>
+            </div>
+           
+        </div>
+    </div>
+</body>
+
+</html>
